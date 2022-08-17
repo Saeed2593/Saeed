4e51f986e8a0940d7c07cbdc451ce8ca95cd23b1
saeed <saeedshaik25293@gmail.com>
2022-08-17 09:45:57 +0530

    First Java Program to just print a text.

---
+++ src/basics/Programming.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Programming {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("sabeha@1999");
+	}
+
+}
