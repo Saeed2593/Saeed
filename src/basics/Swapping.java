5fd6b87f3569be0e53fbb2f104d06d7f1ea899aa
saeed <saeedshaik25293@gmail.com>
2022-08-30 09:23:18 +0530

    Swap 2 varaiables without using 3rd varaiable:

---
+++ src/basics/Swapping.java
@@ -0,0 +1,17 @@
+package basics;
+
+
+public class Swapping {
+	public static void main(String[] args)
+	{
+	int a=10;
+	int b=6;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println(a);
+	System.out.println(b);
+	}
+}
+
+
