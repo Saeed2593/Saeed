905733e6f9e8e6b0b7192fbc7852c5be486d3d0c
saeed <saeed@saeedshaik25293@gmail.com>
2022-09-14 09:37:15 +0530

    reverse an array

---
+++ src/basics/ReverseAnArray.java
@@ -0,0 +1,19 @@
+package basics;
+
+
+public class ReverseAnArray {
+	public static void main(String[] args)
+	{
+	int a[]= {5, 3, 2, 6, 8, 11};
+	System.out.println("Array in reverse order:");
+	
+	for(int i=a.length-1; i>=0; i--)
+	{
+		System.out.println(a[i]);
+	
+	
+	}
+}
+	}
+
+
