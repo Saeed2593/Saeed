f94b42bb75620f925ea0a53f2c67a71c6a58e4a2
saeed <saeed@saeedshaik25293@gmail.com>
2022-09-15 08:27:37 +0530

    To print alphabets form A to Z

---
+++ src/basics/Alphabets.java
@@ -0,0 +1,18 @@
+package basics;
+
+
+public class Alphabets {
+	public static void main(String[] args)
+	{
+	char C;
+	
+	for(C='A'; C <='Z'; C++)
+	{
+		System.out.println(C + "");
+	
+	
+	}
+	}
+}
+
+
