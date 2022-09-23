60efa661c56a899b93f1905a8a07a1f7db51b2b2
saeed <saeed@saeed-Inspiron-3551>
2022-09-23 08:33:26 +0530

    Maximum number in an array

---
+++ src/basics/MaxInArray.java
@@ -0,0 +1,26 @@
+package basics;
+
+
+public class MaxInArray{
+	public static void main(String[] args) {
+	int a[] = {6,8,2,10,12};
+	int max=a[0];
+	for(int i=0; i<a.length; i++)
+	{
+		if(a[i]>max) 
+		{
+			max=a[i];
+		}
+	}
+			System.out.print(max);
+		
+}
+	}
+	
+	
+
+
+
+
+
+
