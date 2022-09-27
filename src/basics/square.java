686a31c3b5f0991fd05aea66777efac15e102232
saeed <saeed@saeedshaik25293@gmail.com>
2022-09-27 09:38:36 +0530

    To find the area of a square using class & Object

---
+++ src/basics/square.java
@@ -0,0 +1,31 @@
+package basics;
+
+
+class square{
+	int a;
+	void area()
+	{
+		System.out.println(a*a);
+		
+		}
+	
+	public static void main(String args[])
+	{
+		square s1=new square();
+		square s2=new square();
+		s1.a=5;
+		s2.a=10;
+		s1.area();
+		s2.area();
+	}
+}
+
+	
+	
+		
+
+
+
+
+
+
